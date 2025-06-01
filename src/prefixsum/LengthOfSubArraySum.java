d425a0ec9456f55b3b1721bf6147365f162650b6
Ningappa-Tambad <ningaraj2t@gmail.com>
2025-06-01 17:17:45 +0530

    Updated code

--- src/prefixsum/LengthOfSubArraySum.java
+++ src/prefixsum/LengthOfSubArraySum.java
@@ -2,16 +2,19 @@ package prefixsum;
 
 public class LengthOfSubArraySum {
 
-    public static int subarraySum(int[] nums) {
+    public static int subarraySum(int[] arr)
+    {
         int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int start = Math.max(0, i - nums[i]);
+        for (int i = 0; i < arr.length; i++) {
+            int start = Math.max(0, i - arr[i]);
             for (int j = start; j <= i; j++) {
-                sum += nums[j];
+                sum += arr[j];
+
+            }
+
 
         }
         return sum;
-
     }
         public static void main(String[] args) {
 
